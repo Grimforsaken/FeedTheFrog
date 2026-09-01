c1bfd4e6d3a8084c1b9b7ef023db4cc74544566c
Grimforsaken <grimforsaken@gmail.com>
2026-09-01 17:15:17 -0500

    Version Frog Band two-thumb prototype

--- frog-band-prototype/app/build.gradle.kts
+++ frog-band-prototype/app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
         applicationId = "com.grimforsaken.frogband"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2-frog-select"
+        versionCode = 3
+        versionName = "0.3-two-thumb"
     }
 }
